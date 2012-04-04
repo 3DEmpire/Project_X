acbbbd01b48e64c06f2f15f4d086f4c2a986a89e
TheOrangeView <venom4levski@abv.bg>
2012-04-04 11:40:14 +0300

    changing all navigations and menus to lists + jquery header

--- Project/protected/views/layouts/main.php
+++ Project/protected/views/layouts/main.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=windows-1251" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="language" content="en" />
 
 	<!-- blueprint CSS framework -->
@@ -15,8 +15,9 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/style.css" />
 	
-	<script src="<?php echo Yii::app()->request->baseUrl; ?>/jquery-1.7.1.min.js" type="text/javascript"></script>
-	<script src="<?php echo Yii::app()->request->baseUrl; ?>/jquery.js" type="text/javascript"></script>
+	<script src="<?php echo Yii::app()->request->baseUrl; ?>/jquery/jquery-1.7.1.min.js" type="text/javascript"></script>
+	<script src="<?php echo Yii::app()->request->baseUrl; ?>/jquery/jquery.js" type="text/javascript"></script>
+	<script src="<?php echo Yii::app()->request->baseUrl; ?>/jquery/jquery.nivo.slider.js" type="text/javascript"></script>
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
@@ -30,36 +31,95 @@
   js.src = "//connect.facebook.net/bg_BG/all.js#xfbml=1";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
+<script type="text/javascript">
+$(window).load(function() {
+    $('#slider').nivoSlider({
+        effect: 'random', // Specify sets like: 'fold,fade,sliceDown'
+        slices: 15, // For slice animations
+        boxCols: 8, // For box animations
+        boxRows: 4, // For box animations
+        animSpeed: 500, // Slide transition speed
+        pauseTime: 10000, // How long each slide will show
+        startSlide: 0, // Set starting Slide (0 index)
+        directionNav: true, // Next & Prev navigation
+        directionNavHide: true, // Only show on hover
+        controlNav: true, // 1,2,3... navigation
+        controlNavThumbs: false, // Use thumbnails for Control Nav
+        controlNavThumbsFromRel: false, // Use image rel for thumbs
+        controlNavThumbsSearch: '.jpg', // Replace this with...
+        controlNavThumbsReplace: '_thumb.jpg', // ...this in thumb Image src
+        keyboardNav: true, // Use left & right arrows
+        pauseOnHover: true, // Stop animation while hovering
+        manualAdvance: false, // Force manual transitions
+        captionOpacity: 0.8, // Universal caption opacity
+        prevText: 'Prev', // Prev directionNav text
+        nextText: 'Next', // Next directionNav text
+        randomStart: true, // Start on a random slide
+        beforeChange: function(){}, // Triggers before a slide transition
+        afterChange: function(){}, // Triggers after a slide transition
+        slideshowEnd: function(){}, // Triggers after all slides have been shown
+        lastSlide: function(){}, // Triggers when last slide is shown
+        afterLoad: function(){} // Triggers when slider has loaded
+    });
+});
+</script>
 <center>
 	<div id="GrandContainer">
-		<div id="Header"></div>
+		<div id="Header">
+			<div id="slider">
+				<img src="../Project/css/img/greece.png" width="999" height="302"/>
+				<img src="../Project/css/img/turkey.png" width="999" height="302"/>
+				<img src="../Project/css/img/sozopol.png" width="999" height="302"/>
+				<img src="../Project/css/img/valensia.png" width="999" height="302"/>
+				<img src="../Project/css/img/italy.png" width="999" height="302"/>
+			</div>
+		</div>
 		<div id="Nav">
-			<div class="ButtonFirst">
-				<div class="ButtonNachalo"><a href="index.php">Начало</a></div>
-			</div>
-			<div class="Button">
-				<div class="ButtonAbout"><a href="#" >За Нас</a></div>
-			</div>
-			<div class="Button">
-				<div class="ButtonLicenz"><a href="#">Лиценз</a></div>
-			</div>
-			<div class="Button2">
-				<div class="ButtonYzZ"><a href="#">Условия за записване</a></div>
-				<div class="ButtonYzZHover"></div>
-			</div>
-			<div class="Button">
-				<div class="ButtonBs"><a href="#">Банкови сметки</a></div>
-			</div>
-			<div class="Button">
-				<div class="ButtonKont"><a ="?r/site/contact">Контакти</a></div>
-			</div>
-			<div class="clearer"></div>
+			<ul class="NavMain">
+				<li class="NavMain_first">
+					<a href="#">
+						<div class="ButtonNachalo">Начало</div>
+					</a>
+				</li>
+				<li class="NavMain">
+					<a href="#">
+						<div class="ButtonAbout">За Нас</div>
+					</a>
+				</li>
+				<li class="NavMain">
+					<a href="#">
+						<div class="ButtonLicenz">Лиценз</div>
+					</a>
+				</li>
+				<li class="NavMain_drop">
+					<a href="#">
+						<div class="ButtonYzZ">Условия за записване</div>
+						<div class="ButtonYzZHover">Условия за записване</div>
+					</a>
+					<ul class="SubMenu">
+						<li>
+							<a href="#"><div class="ButtonYzZ_down1">Договор с клиент за организирано пътуване</div></a>
+							<a href="#"><div class="ButtonYzZ_down1">Договор при пътуване със собствен транспорт</div></a>
+						</li>
+					</ul>
+				</li>
+				<li class="NavMain">
+					<a href="#">
+						<div class="ButtonBs">Банкови сметки</div>
+					</a>
+				</li>
+				<li class="NavMain">
+					<a href="#">
+						<div class="ButtonKont">Контакти</div>
+					</a>
+				</li>
+			</ul>
 		</div>
 		<div id="Main">
-			<div class="ButtonYzZ_down">
+			<!--<div class="ButtonYzZ_down">
 					<div class="ButtonYzZ_down1"><a href="#">Договор с клиент за организирано пътуване</a></div>
 					<div class="ButtonYzZ_down1"><a href="#">Договор при пътуване със собствен транспорт</a></div>
-			</div>
+			</div>-->
 			<div id="Contacts">
 				<div class="Line994"></div>
 				<div id="contactsMain">
@@ -69,66 +129,49 @@
 			</div>
 			<div id="Left">
 				<div id="Menu">
-					<div id="menuMainButs">
-						<a href="#"><div id="menuButTop">
-							<div class="textTop">Начало</div>
-							<div class="icon"></div>
-							<div class="clearer"></div>
-						</div></a>
-						<a href="#" onclick="showPochivki()"><div class="menuBut">
-							<div class="text">Почивки</div>
-							<div class="icon"></div>
-							<div class="clearer"></div>
-						</div></a>
-						<div id="Pochivki_onclick">
-							<div class="poch_on_OffText"><a href="#">България</a></div>
-							<div class="poch_on_OffText"><a href="#">Турция</a></div>
+						<ul id="menuButs">
+							<a href="#"><li id="menuButTop">
+								<span class="textTop">Начало</span>
+							</li></a>
+							<a href="#" onclick="showPochivki()"><li class="menuBut">
+								<span class="text">Почивки</span>
+							</li></a>
+							<div id="Pochivki_onclick">
+										<a href="#"><div class="poch_on_OffText">България</div></a>
+										<a href="#"><div class="poch_on_OffText">Турция</div></a>
 
-							<div class="poch_on_OffText"><a href="#">Гърция</a></div>
+										<a href="#"><div class="poch_on_OffText">Гърция</div></a>
 
-							<div class="poch_on_OffText"><a href="#">Испания</a></div>
+										<a href="#"><div class="poch_on_OffText">Испания</div></a>
 
-							<div class="poch_on_OffText"><a href="#">Италия</a></div>
+										<a href="#"><div class="poch_on_OffText">Италия</div></a>
 
-							<div class="poch_on_OffText"><a href="#">Други страни</a></div>
-
-						</div>
-						<a href="#"><div class="menuBut">
-							<div class="text">Екскурзии</div>
-							<div class="icon"></div>
-							<div class="clearer"></div>
-						</div></a>
-						<a href="#"><div class="menuBut">
-							<div class="text">Екскурзии</div>
-							<div class="icon"></div>
-							<div class="clearer"></div>
-						</div></a>
-						<a href="#"><div class="menuBut">
-							<div class="text">Хотели</div>
-							<div class="icon"></div>
-							<div class="clearer"></div>
-						</div></a>
-						<a href="#"><div class="menuBut">
-							<div class="text">Круизи</div>
-							<div class="icon"></div>
-							<div class="clearer"></div>
-						</div></a>
-							<a href="#"><div class="menuBut">
-							<div class="text">ПРАЗНИЦИ</div>
-							<div class="icon"></div>
-							<div class="clearer"></div>
-						</div></a>
-							<a href="#"><div id="menuButBot">
-							<div class="text">Уикенди</div>
-							<div class="icon"></div>
-							<div class="clearer"></div>
-						</div></a>
-					</div>
-					<div class="menuBlue">
+										<a href="#"><div class="poch_on_OffText">Други страни</div></a>
+							</div>
+							<a href="#"><li class="menuBut">
+								<span class="text">Екскурзии</span>
+							</li></a>
+							<a href="#"><li class="menuBut">
+								<span class="text">Екскурзии</span>
+							</li></a>
+							<a href="#"><li class="menuBut">
+								<span class="text">Хотели</span>
+							</li></a>
+							<a href="#"><li class="menuBut">
+								<span class="text">Круизи</span>
+							</li></a>
+							<a href="#"><li class="menuBut">
+								<span class="text">ПРАЗНИЦИ</span>
+							</li></a>
+							<a href="#"><li id="menuButBot">
+								<span class="text">Уикенди</span>
+							</li></a>
+						</ul>
+				</div>
+				<div class="menuBlue">
 						<div class="menuBlueTop">Нещо</div>
 						<div class="menuBlueMain">asdasdasdasd</div>
 						<div class="menuBlueBot"></div>
-					</div>
 				</div>
 			</div>
 			<div id="Mid">
@@ -141,7 +184,7 @@
 				</div>
 				<div id="MidMain">
 					<div id="offTitle">-Оферти-</div>
-					<?php echo $content; ?>
+					<div id="MidMainPhp"><?php echo $content; ?></div>
 				</div>
 				<div id="MidBot"></div>
 			</div>
@@ -149,66 +192,43 @@
 				<div id="rightBlue">
 					<div id="rightBlueTop"></div>
 					<div id="rightBlueMain">
-						<div class="blueOff">
-							<div class="yellowPointer"></div>
-							<div class="blueOffText"><a href="#">Испания 55+</a></div>
-							<div class="clearer"></div>
-						</div>
-						<div class="blueOff">
-							<div class="yellowPointer"></div>
-							<div class="blueOffText"><a href="#">остров Кипър 55+ Лимасол</a></div>
-							<div class="clearer"></div>
-						</div>
-						<div class="blueOff">
-							<div class="yellowPointer"></div>
-							<div class="blueOffText"><a href="#">остров Кипър 55+ Пафос</a></div>
-							<div class="clearer"></div>
-						</div>
-						<div class="blueOff">
-							<div class="yellowPointer"></div>
-							<div class="blueOffText"><a href="#">Валенсия 55+</a></div>
-							<div class="clearer"></div>
-						</div>
-						<div class="blueOff">
-							<div class="yellowPointer"></div>
-							<div class="blueOffText"><a href="#">Испания 55+</a></div>
-							<div class="clearer"></div>
-						</div>
-						<div class="blueOff">
-							<div class="yellowPointer"></div>
-							<div class="blueOffText"><a href="#">остров Кипър 55+ Лимасол</a></div>
-							<div class="clearer"></div>
-						</div>
-						<div class="blueOff">
-							<div class="yellowPointer"></div>
-							<div class="blueOffText"><a href="#">остров Кипър 55+ Пафос</a></div>
-							<div class="clearer"></div>
-						</div>
-						<div class="blueOff">
-							<div class="yellowPointer"></div>
-							<div class="blueOffText"><a href="#">Валенсия 55+</a></div>
-							<div class="clearer"></div>
-						</div>
-						<div class="blueOff">
-							<div class="yellowPointer"></div>
-							<div class="blueOffText"><a href="#">Испания 55+</a></div>
-							<div class="clearer"></div>
-						</div>
-						<div class="blueOff">
-							<div class="yellowPointer"></div>
-							<div class="blueOffText"><a href="#">остров Кипър 55+ Лимасол</a></div>
-							<div class="clearer"></div>
-						</div>
-						<div class="blueOff">
-							<div class="yellowPointer"></div>
-							<div class="blueOffText"><a href="#">остров Кипър 55+ Пафос</a></div>
-							<div class="clearer"></div>
-						</div>
-						<div class="blueOff">
-							<div class="yellowPointer"></div>
-							<div class="blueOffText"><a href="#">Валенсия 55+</a></div>
-							<div class="clearer"></div>
-						</div>
+						<ul id="TopOferti">
+							<li class="blueOff">
+								<div class="yellowPointer"></div>
+								<div class="blueOffText"><a href="#">Испания 55+</a></div>
+								<div class="clearer"></div>
+							</li>
+							<li class="blueOff">
+								<div class="yellowPointer"></div>
+								<div class="blueOffText"><a href="#">остров Кипър 55+ Лимасол</a></div>
+								<div class="clearer"></div>
+							</li>
+							<li class="blueOff">
+								<div class="yellowPointer"></div>
+								<div class="blueOffText"><a href="#">остров Кипър 55+ Пафос</a></div>
+								<div class="clearer"></div>
+							</li>
+							<li class="blueOff">
+								<div class="yellowPointer"></div>
+								<div class="blueOffText"><a href="#">Валенсия 55+</a></div>
+								<div class="clearer"></div>
+							</li>
+							<li class="blueOff">
+								<div class="yellowPointer"></div>
+								<div class="blueOffText"><a href="#">Испания 55+</a></div>
+								<div class="clearer"></div>
+							</li>
+							<li class="blueOff">
+								<div class="yellowPointer"></div>
+								<div class="blueOffText"><a href="#">остров Кипър 55+ Лимасол</a></div>
+								<div class="clearer"></div>
+							</li>
+							<li class="blueOff">
+								<div class="yellowPointer"></div>
+								<div class="blueOffText"><a href="#">остров Кипър 55+ Пафос</a></div>
+								<div class="clearer"></div>
+							</li>
+						</ul>
 					</div>
 					<div id="rightBlueBot"></div>
 				</div>
@@ -222,7 +242,11 @@
 			</div>
 			<div class="clearer"></div>
 		</div>
-		<div id="Footer">Foooter</div>
+		<div id="Footer">
+			<div class="Line994"></div>
+			<span id="FooterLeft">Ivaniko Tour © 1999-2012 Всички права запазени.</span>
+			<span id="FooterRight">дизайн и програмиране: 3DEmpire</span>
+		</div>
 	</div>
 	</center>
 </body>
